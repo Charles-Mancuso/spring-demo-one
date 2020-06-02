f88ab1ddb66788a3d5543160eafaf29490dd5677
charles mancuso <charlesmancuso66@yahoo.com>
2020-06-01 22:51:14 -0700

    Load spring config file and retrieve bean from spring container

--- spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -1,15 +1,21 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SetterDemoApp {
 
 	public static void main(String[] args) {
 		//Load spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContent.xml");
 		
 		//Retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 		
 		//Call methods on bean
 		
 		//Close the context
+		context.close();
 	}
 
 }
