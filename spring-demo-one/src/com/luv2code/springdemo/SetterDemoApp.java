3eb6bc2ffe43f079c1d347ce2a7a3629044ae314
charles mancuso <charlesmancuso66@yahoo.com>
2020-06-01 22:48:08 -0700

    Add setter demo app class and comments

---
+++ spring-demo-one/src/com/luv2code/springdemo/SetterDemoApp.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		//Load spring configuration file
+		
+		//Retrieve bean from spring container
+		
+		//Call methods on bean
+		
+		//Close the context
+	}
+
+}
