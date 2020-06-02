6c267862199e81c9c9ea0116801b86d0042d925b
charles mancuso <charlesmancuso66@yahoo.com>
2020-06-01 22:39:11 -0700

    Add daily workout and fortune to coach class

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -2,16 +2,16 @@ package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach {
 
+	private FortuneService fortuneService;
+	
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Practice fast bowling for 15 minutes";
 	}
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return fortuneService.getFortune();
 	}
 
 }
